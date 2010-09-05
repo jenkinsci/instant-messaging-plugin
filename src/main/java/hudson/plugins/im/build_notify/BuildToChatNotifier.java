d777f9cfec6bf760a6fc2d3c2d590a80c23fe021
kutzi <kutzi@dev.java.net>
2010-09-05 20:31:12 +0000

    minor

--- src/main/java/hudson/plugins/im/build_notify/BuildToChatNotifier.java
+++ src/main/java/hudson/plugins/im/build_notify/BuildToChatNotifier.java
@@ -10,14 +10,21 @@ import hudson.plugins.im.IMPublisher;
 import java.io.IOException;
 
 /**
- * Controls the exact message to be sent to a chat upon a completion of a build.
+ * Controls the exact messages to be sent to a chat upon start/completion of a build.
  *
  * @author Kohsuke Kawaguchi
  * @since 1.9
  */
 public abstract class BuildToChatNotifier implements Describable<BuildToChatNotifier> {
     /**
-     * Calculates the message to send out to a chatwhen the specified build is started.
+     * Calculates the message to send out to a chat when the specified build is started.
+     * 
+     * @param publisher
+     *      The publisher that's driving this. Never null.
+     * @param build
+     *      The build that just completed. Never null.
+     * @param listener
+     *      Any errors can be reported here.
      */
     public abstract String buildStartMessage(IMPublisher publisher, AbstractBuild<?, ?> build, BuildListener listener)
             throws IOException,  InterruptedException;
